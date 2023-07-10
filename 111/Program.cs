0d52b1dd934ab3c3dbee41847d7e8d4747017f22
Isance793 <qiqidoyou333@gmail.com>
2023-07-10 11:58:25 +0300

    done

--- 111/Program.cs
+++ 111/Program.cs
@@ -1,2 +1,36 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+
+        System.Console.Write("Введите количество элементов массива:");
+
+        int elementsCount = int.Parse(Console.ReadLine());
+
+        int[] array = new int[elementsCount];
+        
+        for (int i = 0; i < array.Length; i++)
+        {
+            System.Console.WriteLine($"Введите элемент массива под индексом {i} ");
+            array[i] = int.Parse(Console.ReadLine());
+        }
+
+        System.Console.WriteLine("Вывод массива");
+
+        for (int i = 0; i < array.Length; i++)
+        {
+            System.Console.WriteLine(array[i]);
+        }
+
+        Random random = new Random();
+
+        for (int i = 0; i < 3; i++)
+        {
+            int randomIndex = random.Next(0, array.Length);
+            System.Console.WriteLine(array[randomIndex]);
+        }
+
+        Console.ReadLine();
+
+    }
+}
